6ebdd16abeab010f12faa8031a519378666c51a4
scarly111 <yigitcagri48@gmail.com>
2025-06-01 18:35:15 +0300

    basic structure

---
+++ linked-list.js
@@ -0,0 +1,12 @@
+class Node {
+  constructor(value = null, nextNode = null) {
+    this.value = value
+    this.nextNode = nextNode
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null
+  }
+}
