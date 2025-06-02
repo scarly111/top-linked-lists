89ef1742f1ce08cfd497a239d3e94571f4d12161
scarly111 <yigitcagri48@gmail.com>
2025-06-02 17:41:12 +0300

    linked list tested

---
+++ main.js
@@ -0,0 +1,13 @@
+// for testing
+const LinkedList = require('./linked-list');
+
+const list = new LinkedList()
+
+list.append("dog")
+list.append("cat")
+list.append("parrot")
+list.append("hamster")
+list.append("snake")
+list.append("turtle")
+
+console.log(list.toString())
